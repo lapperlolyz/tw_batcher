b88d29bd73159318587de0473a17cbf16a1faefe
Lapper <mys4league@mail.ru>
2020-04-07 18:29:50 +0300

    добавил ассеты

--- assets/components/rebatcher/connectors/resources.php
+++ assets/components/rebatcher/connectors/resources.php
@@ -8,73 +8,102 @@ require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/config.core
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
 require_once MODX_CONNECTORS_PATH.'index.php';
 
-$limit = isset($_REQUEST['limit']) ? $_REQUEST['limit'] : 0;
-$offset = isset($_REQUEST['offset']) ? $_REQUEST['offset'] : 0;
-//$search = isset($_REQUEST['search']) ? $_REQUEST['search'] : '';
+$input = json_decode(file_get_contents("php://input"), true);
+$params = $input['data'];
+
+$limit = isset($params['limit']) ? $params['limit'] : 0;
+$offset = isset($params['offset']) ? $params['offset'] : 0;
 
 /** @var modx $modx */
 $query = $modx->newQuery('modResource');
 
-if (isset($_REQUEST['search'])) {
+if (isset($params['context_key'])) {
     $query->where([
-        'pagetitle:LIKE' => "%".$_REQUEST['search']."%"
+        'context_key:=' => $params["context_key"]
     ]);
 }
-if (isset($_REQUEST['context_key'])) {
+if (isset($params['template'])) {
     $query->where([
-        'context_key:=' => $_REQUEST["context_key"]
+        'template:=' => $params['template']
     ]);
 }
-if (isset($_REQUEST['template'])) {
+
+if (isset($params['filters'])) {
+    foreach ($params['filters'] as $f) {
+        $query->where([
+            $f['field'] . ":" . $f['operator'] => $f['value']
+        ]);
+    }
+} else if (isset($params['search'])) {
     $query->where([
-        'template:=' => $_REQUEST['template']
+        'pagetitle:LIKE' => "%".$params['search']."%"
+    ]);
+    $query->orCondition ([
+        'alias:LIKE' => "%".$params['search']."%"
     ]);
 }
+
+if (isset($params['sort'])) {
+    foreach($params['sort'] as $key => $value) {
+        if (strpos($value[0], 'tv_') !== 0) {
+            if (!$value[1]) {
+                $query->sortby($value[0]);
+            } else {
+                $query->sortby($value[0], 'DESC');
+            }
+        }
+    }
+}
+
 $total = $modx->getCount('modResource', $query);
 $query->limit($limit, $limit * $offset);
 
-//$query->leftJoin('modTemplateVarResource', 'TVR', 'TVR.contentid = modResource.id');
-//$query->leftJoin('modTemplateVar', 'TV', 'TV.id = TVR.tmplvarid');
-
 $res = $modx->getCollection('modResource', $query);
 
-//$query->select([
-//    'modResource.*',
-//    'TVR.*',
-//    'TV.*'
-//]);
-//
-//if ($query->prepare() && $query->stmt->execute()) {
-//    $output = $query->stmt->fetchAll(PDO::FETCH_ASSOC);
-//}
-
 $data = [];
-
-//foreach($res as $r) {
-//    $arr = $r->toArray();
-//    $output[] = $arr;
-//}
 foreach($res as $r) {
     $arr = $r->toArray();
-    $tvs = $r->getMany('modTemplateVar');
 
-    $tvarr = [];
-    foreach ($tvs as $tv) {
-        $id = $tv->get('id');
-        $tvname = $tv->get('name');
-        $tvarr[] = [
-            'id' => $id,
-            'name' => $tvname,
-            'value' => $r->getTVValue($id)
-        ];
-        foreach($tvarr as $tvv) {
-            $arr["tv-$tvname"] = $tvv['value'];
+    if (isset($params['tvs'])) {
+//        $tvs = $r->getMany('modTemplateVar');
+
+        $tvs = $modx->getCollection('modTemplateVar', [
+            'name:IN' => $params['tvs']
+        ]);
+
+        $tvarr = [];
+        foreach ($tvs as $tv) {
+            $id = $tv->get('id');
+            $tvname = $tv->get('name');
+            $tvarr[] = [
+                'id' => $id,
+                'name' => $tvname,
+                'value' => $r->getTVValue($id)
+            ];
+            foreach ($tvarr as $tvv) {
+                $arr["tv_$tvname"] = $tvv['value'];
+            }
         }
+        $arr['tvs'] = $tvarr;
     }
-    $arr['tvs'] = $tvarr;
 
     $data[] = $arr;;
 }
+
+if (isset($params['sort'])) {
+    foreach($params['sort'] as $key => $value) {
+        if (strpos($value[0], 'tv_') === 0) {
+            usort($data, function($item1, $item2) use ($value) {
+                if (!$value[1]) {
+                    return $item1[$value[0]] < $item2[$value[0]] ? -1 : 1;
+                } else {
+                    return $item1[$value[0]] < $item2[$value[0]] ? 1 : -1;
+                }
+            });
+        }
+    }
+}
+
 
 $output = [
     'total' => $total,
